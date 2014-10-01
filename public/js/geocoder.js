fffa5353eda0d4d03e3efaa2bd1fa5417793656f
AndersEverett <anders.e.howerton@gmail.com>
2014-10-01 06:23:27 -0700

    working ajax and active record query

--- public/js/geocoder.js
+++ public/js/geocoder.js
@@ -2,13 +2,24 @@
 
   function initialize() {
     geocoder = new google.maps.Geocoder();
+    var that = this;
+    $.ajax({
+      url: '/soldiers',
+      type: 'get',
+      data: $(that).serialize()
+    }).done(function(data) {
+        codeAddress(i[0],i[1])
+      console.log(data.length)
+      console.log(data[0][0])
+      console.log(data[0][1])
+    }).fail(function() {
 
-    $ajax
+    });
     
   }
 
-  function codeAddress() {
-    var address = document.getElementById("address").value;
+  function codeAddress(address) {
+    // var address = document.getElementById("address").value;
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
